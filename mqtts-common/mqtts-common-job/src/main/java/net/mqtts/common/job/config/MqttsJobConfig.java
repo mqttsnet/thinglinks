355c76777545e69cdf03c7ff5f3bd5bf3757efac
shisen <shisen16@163.com>
2021-11-24 09:10:06 +0800

    集成xxl-job定时任务

---
+++ mqtts-common/mqtts-common-job/src/main/java/net/mqtts/common/job/config/MqttsJobConfig.java
@@ -0,0 +1,76 @@
+package net.mqtts.common.job.config;
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * job config
+ *
+ * @author shisen
+ */
+@Configuration
+public class MqttsJobConfig {
+    private Logger logger = LoggerFactory.getLogger(MqttsJobConfig.class);
+
+    @Value("${mqtts.job.admin.addresses}")
+    private String adminAddresses;
+
+    @Value("${mqtts.job.accessToken}")
+    private String accessToken;
+
+    @Value("${mqtts.job.executor.appname}")
+    private String appname;
+
+    @Value("${mqtts.job.executor.address}")
+    private String address;
+
+    @Value("${mqtts.job.executor.ip}")
+    private String ip;
+
+    @Value("${mqtts.job.executor.port}")
+    private int port;
+
+    @Value("${mqtts.job.executor.logpath}")
+    private String logPath;
+
+    @Value("${mqtts.job.executor.logretentiondays}")
+    private int logRetentionDays;
+
+    @Bean
+    public XxlJobSpringExecutor xxlJobExecutor() {
+        logger.info(">>>>>>>>>>> xxl-job config init.");
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
+        xxlJobSpringExecutor.setAppname(appname);
+        xxlJobSpringExecutor.setAddress(address);
+        xxlJobSpringExecutor.setIp(ip);
+        xxlJobSpringExecutor.setPort(port);
+        xxlJobSpringExecutor.setAccessToken(accessToken);
+        xxlJobSpringExecutor.setLogPath(logPath);
+        xxlJobSpringExecutor.setLogRetentionDays(logRetentionDays);
+        return xxlJobSpringExecutor;
+    }
+
+    /**
+     * 针对多网卡、容器内部署等情况，可借助 "spring-cloud-commons" 提供的 "InetUtils" 组件灵活定制注册IP；
+     *
+     *      1、引入依赖：
+     *          <dependency>
+     *             <groupId>org.springframework.cloud</groupId>
+     *             <artifactId>spring-cloud-commons</artifactId>
+     *             <version>${version}</version>
+     *         </dependency>
+     *
+     *      2、配置文件，或者容器启动变量
+     *          spring.cloud.inetutils.preferred-networks: 'xxx.xxx.xxx.'
+     *
+     *      3、获取IP
+     *          String ip_ = inetUtils.findFirstNonLoopbackHostInfo().getIpAddress();
+     */
+
+
+}
