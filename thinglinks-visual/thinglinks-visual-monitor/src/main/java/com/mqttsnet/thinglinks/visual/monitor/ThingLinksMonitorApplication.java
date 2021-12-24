e4232e8320a8eda76c9548b52f0b012c07364720
sunshihuan <13733918655@163.com>
2021-12-24 16:24:02 +0800

    readme

--- thinglinks-visual/thinglinks-visual-monitor/src/main/java/com/mqttsnet/thinglinks/visual/monitor/ThingLinksMonitorApplication.java
+++ thinglinks-visual/thinglinks-visual-monitor/src/main/java/com/mqttsnet/thinglinks/visual/monitor/ThingLinksMonitorApplication.java
@@ -16,15 +16,6 @@ public class ThingLinksMonitorApplication
     public static void main(String[] args)
     {
         SpringApplication.run(ThingLinksMonitorApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  监控中心启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
+        System.out.println("(♥◠‿◠)ﾉﾞ  监控中心启动成功   ლ(´ڡ`ლ)ﾞ  ");
     }
 }
