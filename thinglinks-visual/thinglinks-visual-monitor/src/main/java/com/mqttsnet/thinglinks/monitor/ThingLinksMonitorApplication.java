b5be6748718ff1a9ba0bc7e1a0ed448b0cda1382
sunshihuan <13733918655@163.com>
2022-01-04 18:47:13 +0800

    优化设备管理页面

--- thinglinks-visual/thinglinks-visual-monitor/src/main/java/com/mqttsnet/thinglinks/monitor/ThingLinksMonitorApplication.java
+++ thinglinks-visual/thinglinks-visual-monitor/src/main/java/com/mqttsnet/thinglinks/monitor/ThingLinksMonitorApplication.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public class ThingLinksMonitorApplication {
     public static void main(String[] args) {
         SpringApplication.run(ThingLinksMonitorApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  监控中心服务启动成功   ლ(´ڡ`ლ)ﾞ  ");
     }
 
 }
