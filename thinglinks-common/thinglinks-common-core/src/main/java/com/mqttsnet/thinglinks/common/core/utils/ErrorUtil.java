00d9345a1f8b551f71fd0010b39ee74340b2f683
shisen <shisen16@163.com>
2022-07-08 17:06:58 +0800

    增加websocket

---
+++ thinglinks-common/thinglinks-common-core/src/main/java/com/mqttsnet/thinglinks/common/core/utils/ErrorUtil.java
@@ -0,0 +1,26 @@
+package com.mqttsnet.thinglinks.common.core.utils;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ * 捕获报错日志处理工具类
+ */
+public class ErrorUtil {
+
+    /**
+     * Exception出错的栈信息转成字符串
+     * 用于打印到日志中
+     */
+    public static String errorInfoToString(Throwable e) {
+        //try-with-resource语法糖 处理机制
+        try(StringWriter sw = new StringWriter();PrintWriter pw = new PrintWriter(sw)){
+            e.printStackTrace(pw);
+            pw.flush();
+            sw.flush();
+            return sw.toString();
+        }catch (Exception ignored){
+            throw new RuntimeException(ignored.getMessage(),ignored);
+        }
+    }
+}
---
+++ thinglinks-modules/thinglinks-modules-broker/src/main/java/com/mqttsnet/thinglinks/websocket/LoggingWSServer.java
@@ -0,0 +1,186 @@
+package com.mqttsnet.thinglinks.websocket;
+
+import com.mqttsnet.thinglinks.common.core.utils.ErrorUtil;
+import com.mqttsnet.thinglinks.common.core.utils.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * WebSocket获取实时日志并输出到Web页面
+ */
+@Slf4j
+@Component
+@ServerEndpoint(value = "/websocket/logging", configurator = MyEndpointConfigure.class)
+public class LoggingWSServer {
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Autowired
+    AsyncTaskExecutor asyncTaskExecutor;
+
+    /**
+     * 连接集合
+     */
+    private static Map<String, Session> sessionMap = new ConcurrentHashMap<>(3);
+    private static Map<String, Integer> lengthMap = new ConcurrentHashMap<>(3);
+
+    /**
+     * 匹配日期开头加换行，2019-08-12 14:15:04
+     */
+    private Pattern datePattern = Pattern.compile("[\\d+][\\d+][\\d+][\\d+]-[\\d+][\\d+]-[\\d+][\\d+] [\\d+][\\d+]:[\\d+][\\d+]:[\\d+][\\d+]");
+
+
+    /**
+     * 连接建立成功调用的方法
+     */
+    @OnOpen
+    public void onOpen(Session session) {
+        //添加到集合中
+        sessionMap.put(session.getId(), session);
+        lengthMap.put(session.getId(), 1);//默认从第一行开始
+
+        //获取日志信息
+        asyncTaskExecutor.submit(() -> {
+            log.info("LoggingWebSocketServer 任务开始");
+            boolean first = true;
+            BufferedReader reader = null;
+            FileReader fileReader = null;
+            while (sessionMap.get(session.getId()) != null) {
+                try {
+                    //日志文件，获取最新的
+//                    fileReader = new FileReader(System.getProperty("user.home") + "/logs/" + new SimpleDateFormat("yyyyMMdd").format(new Date()) + "/" + applicationName + ".log");
+                    fileReader = new FileReader(System.getProperty("user.dir") + "/logs/thinglinks-broker" + "/" + "info" + ".log");
+
+                    //字符流
+                    reader = new BufferedReader(fileReader);
+                    Object[] lines = reader.lines().toArray();
+
+                    //只取从上次之后产生的日志
+                    Object[] copyOfRange = Arrays.copyOfRange(lines, lengthMap.get(session.getId()), lines.length);
+
+                    //对日志进行着色，更加美观  PS：注意，这里要根据日志生成规则来操作
+                    for (int i = 0; i < copyOfRange.length; i++) {
+                        String line = String.valueOf(copyOfRange[i]);
+                        //先转义
+                        line = line.replaceAll("&", "&amp;")
+                                .replaceAll("<", "&lt;")
+                                .replaceAll(">", "&gt;")
+                                .replaceAll("\"", "&quot;");
+
+                        //处理等级
+                        line = line.replace("DEBUG", "<span style='color: blue;'>DEBUG</span>");
+                        line = line.replace("INFO", "<span style='color: green;'>INFO</span>");
+                        line = line.replace("WARN", "<span style='color: orange;'>WARN</span>");
+                        line = line.replace("ERROR", "<span style='color: red;'>ERROR</span>");
+
+                        //处理类名
+                        String[] split = line.split("]");
+                        if (split.length >= 2) {
+                            String[] split1 = split[1].split("-");
+                            if (split1.length >= 2) {
+                                line = split[0] + "]" + "<span style='color: #298a8a;'>" + split1[0] + "</span>" + "-" + split1[1];
+                            }
+                        }
+
+                        // 匹配日期开头加换行，2019-08-12 14:15:04
+                        Matcher m = datePattern.matcher(line);
+                        if (m.find()) {
+                            //找到下标
+                            int start = m.start();
+                            //插入
+                            StringBuilder sb = new StringBuilder(line);
+                            sb.insert(start, "<br/><br/>");
+                            line = sb.toString();
+                        }
+
+                        copyOfRange[i] = line;
+                    }
+
+                    //存储最新一行开始
+                    lengthMap.replace(session.getId(), lines.length);
+
+                    //第一次如果太大，截取最新的200行就够了，避免传输的数据太大
+                    if (first && copyOfRange.length > 200) {
+                        copyOfRange = Arrays.copyOfRange(copyOfRange, copyOfRange.length - 200, copyOfRange.length);
+                        first = false;
+                    }
+
+                    String result = StringUtils.join(copyOfRange, "<br/>");
+
+                    //发送
+                    send(session, result);
+
+                    //休眠一秒
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    //输出到日志文件中
+                    log.error(ErrorUtil.errorInfoToString(e));
+                }
+            }
+            try {
+                reader.close();
+                fileReader.close();
+            } catch (IOException e) {
+                //输出到日志文件中
+                log.error(ErrorUtil.errorInfoToString(e));
+            }
+            log.info("LoggingWebSocketServer 任务结束");
+        });
+    }
+
+    /**
+     * 连接关闭调用的方法
+     */
+    @OnClose
+    public void onClose(Session session) {
+        //从集合中删除
+        sessionMap.remove(session.getId());
+        lengthMap.remove(session.getId());
+    }
+
+    /**
+     * 发生错误时调用
+     */
+    @OnError
+    public void onError(Session session, Throwable error) {
+        //输出到日志文件中
+        log.error(ErrorUtil.errorInfoToString(error));
+    }
+
+    /**
+     * 服务器接收到客户端消息时调用的方法
+     */
+    @OnMessage
+    public void onMessage(String message, Session session) {
+
+    }
+
+    /**
+     * 封装一个send方法，发送消息到前端
+     */
+    private void send(Session session, String message) {
+        try {
+            session.getBasicRemote().sendText(message);
+        } catch (Exception e) {
+            //输出到日志文件中
+            log.error(ErrorUtil.errorInfoToString(e));
+        }
+    }
+}
