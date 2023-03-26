aea1aff64fd467f3e7952cdfe71953c1407fad2f
师森 <shisen16@163.com>
2023-03-26 16:23:42 +0800

    优化项目配置文件配置繁琐问题，把公用文件提取出来

--- thinglinks-visual/thinglinks-visual-monitor/src/main/java/com/mqttsnet/thinglinks/monitor/ThingLinksMonitorApplication.java
+++ thinglinks-visual/thinglinks-visual-monitor/src/main/java/com/mqttsnet/thinglinks/monitor/ThingLinksMonitorApplication.java
@@ -1,19 +1,12 @@
 package com.mqttsnet.thinglinks.monitor;
 
-import com.mqttsnet.thinglinks.common.swagger.annotation.EnableCustomSwagger2;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@EnableCustomSwagger2
-@SpringBootApplication(scanBasePackages = {"com.mqttsnet.thinglinks"})
-@MapperScan("com.mqttsnet.thinglinks.monitor.mapper")
-@EnableCaching
-@EnableScheduling
-@CrossOrigin(origins = "*",maxAge = 3600)
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
+@SpringBootApplication
 public class ThingLinksMonitorApplication {
     public static void main(String[] args) {
         SpringApplication.run(ThingLinksMonitorApplication.class, args);
