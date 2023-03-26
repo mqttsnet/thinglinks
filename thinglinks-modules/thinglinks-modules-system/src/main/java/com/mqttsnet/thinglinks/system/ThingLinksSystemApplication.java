524ebdf4471f24988b789ab3f86c89e050ba1f11
师森 <shisen16@163.com>
2023-03-26 22:05:53 +0800

    fix 提取服务端口到配置中心,td排除seate

--- thinglinks-modules/thinglinks-modules-system/src/main/java/com/mqttsnet/thinglinks/system/ThingLinksSystemApplication.java
+++ thinglinks-modules/thinglinks-modules-system/src/main/java/com/mqttsnet/thinglinks/system/ThingLinksSystemApplication.java
@@ -5,6 +5,7 @@ import com.mqttsnet.thinglinks.common.security.annotation.EnableRyFeignClients;
 import com.mqttsnet.thinglinks.common.swagger.annotation.EnableCustomSwagger2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
@@ -20,7 +21,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 //maxAge飞行前响应的缓存持续时间的最大年龄，简单来说就是Cookie的有效期 单位为秒
 //若maxAge是负数,则代表为临时Cookie,不会被持久化,Cookie信息保存在浏览器内存中,浏览器关闭Cookie就消失
 @CrossOrigin(origins = "*",maxAge = 3600)
-@SpringBootApplication(scanBasePackages = {"com.mqttsnet.thinglinks"})
+@SpringBootApplication(exclude = {DataSourceTransactionManagerAutoConfiguration.class},scanBasePackages = {"com.mqttsnet.thinglinks"})
 public class ThingLinksSystemApplication
 {
     public static void main(String[] args)
