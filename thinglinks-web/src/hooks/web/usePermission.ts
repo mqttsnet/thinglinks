94e160517db73772866ac723fbffea02713792fc
xiaonan <13733918655@163.com>
2026-07-18 17:09:10 +0800

    fix(web): 避免重复注册未找到路由
    同步自 Pro 提交 5c206b4173d604e8f7dacf7072ba6276a662677b。
    
    Co-authored-by: Ab <95611581+whb-11@users.noreply.github.com>

--- thinglinks-web/src/hooks/web/usePermission.ts
+++ thinglinks-web/src/hooks/web/usePermission.ts
@@ -160,9 +160,9 @@ export function usePermission() {
     resetRouter();
     const routes = await permissionStore.buildRoutesAction();
     routes.forEach((route) => {
+      if (route.name === PAGE_NOT_FOUND_ROUTE.name) return;
       router.addRoute(route as unknown as RouteRecordRaw);
     });
-    router.addRoute(PAGE_NOT_FOUND_ROUTE as unknown as RouteRecordRaw);
     permissionStore.setLastBuildMenuTime();
     closeAll();
   }
